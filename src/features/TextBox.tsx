2f76537df261bdf9f18f2e28e4242fa32e85273a
clark whitehead <clark12341@gmail.com>
2022-06-18 13:08:17 -1000

    test

--- src/features/TextBox.tsx
+++ src/features/TextBox.tsx
@@ -4,17 +4,23 @@ export const TextBox = () => {
 
     const [text, setText] = useState("");
 
+    const handleTextChange = (event) => {
+        setText(event.target.value);
+    }
+
     return (
         <div className="container">
             <div className="row">
                 <div className="col-8 m-auto">
-                    <input 
+                    <input
+                        onChange={handleTextChange}
                         style={{height: 50, fontSize: 15}}
                         enterKeyHint="send" 
                         type="text" 
                         autoFocus
                         className="w-100 pl-3"
                         placeholder="Type your message..."
+                        value={text}
                     />
                 </div>
                 <button className="col-4 btn btn-primary">
