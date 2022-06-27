8cea65b0c63a74be214fc7104c0cf1594895ae45
clark whitehead <clark12341@gmail.com>
2022-06-27 11:32:03 -1000

    new logo size

--- src/app/App.js
+++ src/app/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
       <div className="App">
         <Navbar />
-        <div className="container">
+        <div className="container pb-20">
           <Logo />
           <ConversationBox />          
           <TextBox />
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div className="mt-5">
-            <img width="55%" className="rounded" src="ai_tutor.png" />
+            <img width="55%" className="rounded" src="ai_tutor_lana.png" />
             <h4 style={{fontFamily: 'monospace'}} className="text-dark">The AI Tutor for any Subject</h4>
         </div>
     )
