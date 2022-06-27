c419dcf82eb139404db09a63dce976218d60ce52
clark whitehead <clark12341@gmail.com>
2022-06-27 11:12:15 -1000

    smaller logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 export const Logo = () => {
     return (
         <div className="mt-5">
-            <img width="75%" className="rounded" src="ai_tutor.png" />
+            <img width="55%" className="rounded" src="ai_tutor.png" />
             <h4 style={{fontFamily: 'monospace'}} className="text-dark">The AI Tutor for any Subject</h4>
         </div>
     )
