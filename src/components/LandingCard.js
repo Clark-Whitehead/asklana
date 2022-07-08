216a255d766f8237d6b1ff109b087ab14f59d693
Clark Whitehead <clark12341@gmail.com>
2022-07-08 12:37:21 -0700

    improve cards

--- src/components/LandingCard.js
+++ src/components/LandingCard.js
@@ -1,8 +1,16 @@
 export const LandingCard = (props) => {
     return (
-        <div style={{height: '100px'}} className="col-4">
-            <div style={{borderRadius: '10px', height: '300px'}} className="border border-dark">
-                <h1>{props.title}</h1>
+        <div className="col-4">
+            <div style={{borderRadius: '10px'}} className="border border-dark pb-4 px-2 h-100">
+                <img width="100%" src={props.image} className="p-2" />
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+                <a href="#">
+                    <span style={{fontWeight: 'bold', color: '#047090'}}>
+                        Learn more <i class="bi bi-arrow-right"></i>
+                    </span>
+                </a>
+                    
             </div>
         </div>
     )
--- src/components/LandingCards.js
+++ src/components/LandingCards.js
@@ -1,11 +1,16 @@
 import { LandingCard } from "./LandingCard"
 
 export const LandingCards = () => {
+
+    const description1 = "Stuff is going good Stuff is going good Stuff is going good Stuff is going good"
+    const description2 = "Stuff is going good"
+    const description3 = "Stuff is going good"
+
     return (
-        <div style={{height: "300px"}} className="row mt-4 mb-5">
-            <LandingCard title="hey" />
-            <LandingCard title="There"/>
-            <LandingCard title="dude"/>
+        <div className="row mt-4 mb-5">
+            <LandingCard title="hey" image="https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg" description={description1} />
+            <LandingCard title="There" image="https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg" description={description2} />
+            <LandingCard title="dude" image="https://www.coderus.com/wp-content/uploads/2020/11/different-types-of-software-coderus-branded-image.jpg" description={description3} />
         </div>
     )
 }
