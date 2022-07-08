7dc24b227af63febc203155fdc90c3104d81d84c
Clark Whitehead <clark12341@gmail.com>
2022-07-08 00:57:47 -0700

    progress

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -11,6 +11,28 @@ export const Landing = () => {
     })
 
     return (
-        <h1>Landing Page</h1>
+        <div style={{width: '70vw', marginLeft: 'auto', marginRight: 'auto'}} className="container">
+            <div className="row">
+                <div style={{border: 'solid', borderColor: 'orange'}} className='col-6 rounded p-1 mb-3 mx-auto'>
+                    <div className="row">
+                        <div className="col-9 pl-3 pr-1">
+                            <input placeholder='Get an answer...' type="text" style={{border: 0, outline: 'none', height: '100%', width: '100%'}} />
+                        </div>
+                        <div className="col-3 pr-3 pl-0">
+                            <button style={{fontWeight: 'bold', backgroundColor: '#F18000', color: 'white'}} className="btn w-100">Ask</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div style={{backgroundImage: "url(/chalkboard.png)", backgroundRepeat: 'no-repeat', height: '100%', backgroundSize: "100%", width: '100%', fontFamily: 'Fredericka the Great', color: 'white'}} className="row-fluid">
+                <div className="col-4 text-left">
+
+                    
+                    <h5 style={{fontSize: '1.25em'}}>24/7 COURSE SUPPORT</h5>
+                    <h1>Learn with Lana</h1>
+
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,30 +17,33 @@ export const Navbar = () => {
 
     return (
         
-        <nav style={{backgroundColor: "#add8e6", marginBottom: 0}} className="navbar navbar-expand-md navbar-dark">
-            <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
-                <ul className="navbar-nav mr-auto">
-                    <li style={{marginRight: '10px'}} className="nav-item">
-                        <a style={{color: "#006C81", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/">HOME</a>
-                    </li>
-                    <li style={{marginRight: '10px'}} className="nav-item">
-                        <a style={{color: "#006C81", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/conversations">SHARED CONVERSATIONS</a>
-                    </li>
-                </ul>
-            </div>
+        <nav style={{marginBottom: 0}} className="navbar navbar-expand-md navbar-dark">
             <div className="mx-auto order-0">
-                <a style={{color: "#0B043D", fontFamily: 'cursive', fontSize: '25px', fontWeight: 'bold'}} className="navbar-brand mx-auto" href="#">"Learning is Power" - Lana</a>
+                <a style={{color: "#F18000", fontFamily: 'Fredericka the Great', fontSize: '25px', fontWeight: 'bold'}} className="navbar-brand mx-auto" href="/">ASKLANA</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
                     <span className="navbar-toggler-icon"></span>
                 </button>
             </div>
+            <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+                <ul className="navbar-nav m-auto">
+                    <li style={{marginRight: '10px'}} className="nav-item">
+                        <a style={{color: "black", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/">Math</a>
+                    </li>
+                    <li style={{marginRight: '10px'}} className="nav-item">
+                        <a style={{color: "black", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/conversations">Chemistry</a>
+                    </li>
+                    <li style={{marginRight: '10px'}} className="nav-item">
+                        <a style={{color: "black", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/conversations">Software</a>
+                    </li>
+                    <li style={{marginRight: '10px'}} className="nav-item">
+                        <a style={{color: "black", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/conversations">Physics</a>
+                    </li>
+                </ul>
+            </div>
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2">
                 {!user && <ul className="navbar-nav ml-auto">
                     <li style={{marginRight: '10px'}} className="nav-item">
-                        <a style={{color: "#006C81", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/login">LOGIN</a>
-                    </li>
-                    <li style={{marginRight: '10px'}} className="nav-item">
-                        <a style={{color: "#006C81", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/register">REGISTER</a>
+                        <a style={{color: "black", fontFamily: 'arial', fontWeight: 'bold'}} className="nav-link" href="/login">Sign in</a>
                     </li>
                 </ul>}
                 {user && <ul className="navbar-nav ml-auto">
